19bc40b7338bf77410f2ae33e1f35e5f727230e0
@satoshi_m8a <satoshi.m8a@gmail.com>
2014-02-07 20:15:51 +0900

    first commit

---
+++ src/main/g8/build.sbt
@@ -0,0 +1,7 @@
+// give the user a nice default project!
+
+name : = "$name"
+
+version := "$version"
+
+scalaVersion := "$scalaVersion"
---
+++ src/main/g8/src/main/scala/$/$.scala
@@ -0,0 +1,7 @@
+package $organization$
+
+def $name;format="Camel"$ {
+		def main(args:Array[String]):Unit = {
+				println("run")
+		}
+}
